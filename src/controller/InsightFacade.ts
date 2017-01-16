bb561a0ad4b626994873940dca932466318c8a68
Alice <alicefoster17@gmail.com>
2017-01-16 11:44:58 -0800

    Added helper stub, fulfill/reject conditions and comments

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -7,14 +7,21 @@ import Log from "../Util";
 
 export default class InsightFacade implements IInsightFacade {
 
+
     constructor() {
         Log.trace('InsightFacadeImpl::init()');
     }
 
+    // Helper function, needs implementation
+    dataAlreadyExists(id: string) {
+
+    }
+
+    // Content = zip data
+    // id = id of the data being added
     addDataset(id: string, content: string): Promise<InsightResponse> {
         Log.trace("Inside addDataset()");
-        // Content = zip data
-        // id = id of the data being added
+        let that = this;
         return new Promise(function(fulfill, reject) {
             var rp = require("request-promise-native");
             rp.debug = true;
@@ -25,9 +32,24 @@ export default class InsightFacade implements IInsightFacade {
             rp(options)
                 .then(function(htmlString: any) {
                     Log.trace("Promise returned successfully, htmlString = " + htmlString);
-                    fulfill(htmlString);
+
+                    /* Fulfill conditions:
+                        * 201: the operation was successful and the id already existed (was added in
+                        this session or was previously cached).
+                        * 204: the operation was successful and the id was new (not added in this
+                        session or was previously cached).
+                    */
+                    if(that.dataAlreadyExists(id)) {
+                        fulfill("201");
+                    } else {
+                        fulfill("204");
+                    }
                 })
                 .catch(function(err: any) {
+                    /* Needs to reject the proper errors:
+                        * 400: the operation failed. The body should contain {"error": "my text"}
+                        to explain what went wrong.
+                    */
                     Log.trace("Promise failed to return, err = " + err);
                     reject(err);
                 });
@@ -43,4 +65,5 @@ export default class InsightFacade implements IInsightFacade {
         Log.trace("Inside performQuery");
         return null;
     }
+
 }
