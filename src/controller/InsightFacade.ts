07ed8444f26bba4deb67b0de2747ba9048cb1508
Alice <alicefoster17@gmail.com>
2017-01-16 11:27:18 -0800

    Added debug prints and  basic promise functionality for addDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -12,14 +12,35 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
-        return null;
+        Log.trace("Inside addDataset()");
+        // Content = zip data
+        // id = id of the data being added
+        return new Promise(function(fulfill, reject) {
+            var rp = require("request-promise-native");
+            rp.debug = true;
+            var options = {
+                json: false,
+                uri: content
+            };
+            rp(options)
+                .then(function(htmlString: any) {
+                    Log.trace("Promise returned successfully, htmlString = " + htmlString);
+                    fulfill(htmlString);
+                })
+                .catch(function(err: any) {
+                    Log.trace("Promise failed to return, err = " + err);
+                    reject(err);
+                });
+        });
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
+        Log.trace("Inside removeDataset()");
         return null;
     }
 
     performQuery(query: QueryRequest): Promise <InsightResponse> {
+        Log.trace("Inside performQuery");
         return null;
     }
 }
