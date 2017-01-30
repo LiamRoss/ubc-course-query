cf986294ba07f5ee87d529c56df192df8ae26681
alicelf <alicefoster17@gmail.com>
2017-01-30 12:03:21 -0800

    Removed data folder, using home directory instead

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -109,8 +109,8 @@ export default class InsightFacade implements IInsightFacade {
 
     writeToDisk(id: string) {
         let that = this;
-        fs.writeFileSync("data/" + id + ".json", JSON.stringify(that.dataSets[id]));
-        Log.trace(id + ".json created in data/");
+        fs.writeFileSync(id + ".json", JSON.stringify(that.dataSets[id]));
+        Log.trace(id + ".json created");
     }
 
     /**
